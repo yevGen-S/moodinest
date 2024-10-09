ed8cec954df9960e7e5673d93783dc743a5caa3f
yevGen-S <yevgeniysudakov@gmail.com>
2024-10-09 21:18:04 +0300

    chore: initial structure

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Index() {
+export default function App() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-            }}
-        >
+        <View style={styles.container}>
             <Text>MoodiNest</Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
