ce46ca2c5943ef4cd286347b7f1bd0d6381467d1
yevGen-S <yevgeniysudakov@gmail.com>
2024-10-10 17:39:08 +0300

    refactor: move tab icon to components

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,47 +1,7 @@
-import { View, Image, ImageURISource, Text } from 'react-native';
 import React from 'react';
 import { Tabs } from 'expo-router';
 import icons from '@/constants/icons';
-
-type TabIconProps = {
-    icon: ImageURISource;
-    color: string;
-    name: string;
-    focused?: boolean;
-};
-
-const TabIcon = ({ icon, color, name }: TabIconProps) => {
-    return (
-        <View
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-            }}
-        >
-            <Image
-                source={icon}
-                resizeMode="contain"
-                tintColor={color}
-                style={{
-                    width: 28,
-                    height: 28,
-                    marginBottom: 5,
-                }}
-            />
-            <Text
-                style={{
-                    fontFamily: 'Work Sans',
-                    fontSize: 12,
-                    fontWeight: 500,
-                    lineHeight: 16,
-                    textAlign: 'center',
-                }}
-            >
-                {name}
-            </Text>
-        </View>
-    );
-};
+import { TabIcon } from '@/components/TabIcon/TabIcon';
 
 const TabsLayout = () => {
     return (
