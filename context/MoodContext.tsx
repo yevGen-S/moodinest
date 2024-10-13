87646aaf6b13dbf29db3b0c3b88406b85d28c6f3
Val-Potitova <valpotitova@gmail.com>
2024-10-13 20:18:56 +0300

    fix: added data

--- context/MoodContext.tsx
+++ context/MoodContext.tsx
@@ -3,6 +3,7 @@ import React, { createContext, useState } from 'react';
 interface MoodContextProps {
     mood: number | null;
     isToday: boolean;
+    userID: number;
     setMood: (mood: number) => void;
     setIsToday: (isToday: boolean) => void;
 }
@@ -10,16 +11,18 @@ interface MoodContextProps {
 export const MoodContext = createContext<MoodContextProps>({
     mood: null,
     isToday: false,
+    userID: 0,
     setMood: () => {},
-    setIsToday: () => {},
+    setIsToday: () => {}
 });
 
 export const MoodProvider = ({ children }: { children: React.ReactNode }) => {
     const [mood, setMood] = useState<number | null>(null);
     const [isToday, setIsToday] = useState<boolean>(false);
+    const [userID, setUserID] = useState<number>(0);
 
     return (
-        <MoodContext.Provider value={{ mood, isToday, setMood, setIsToday }}>
+        <MoodContext.Provider value={{ mood, isToday, userID, setMood, setIsToday}}>
             {children}
         </MoodContext.Provider>
     );
