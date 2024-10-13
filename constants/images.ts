1158690ff4dc226c52db469e4c6047ed0bec28bb
Val-Potitova <valpotitova@gmail.com>
2024-10-13 12:16:10 +0300

    import images

--- constants/images.ts
+++ constants/images.ts
@@ -1,5 +1,25 @@
 import meditation from '../assets/images/meditation.png';
+import crying from '../assets/images/crying.png';
+import cryingColor from '../assets/images/cryingColor.png';
+import sadFace from '../assets/images/sadFace.png';
+import sadFaceColor from '../assets/images/sadFaceColor.png';
+import neutral from '../assets/images/neutral.png';
+import neutralColor from '../assets/images/neutralColor.png';
+import smile from '../assets/images/smile.png';
+import smileColor from '../assets/images/smileColor.png';
+import happy from '../assets/images/happy.png';
+import happyColor from '../assets/images/happyColor.png';
 
 export default {
     meditation,
+    crying,
+    cryingColor,
+    sadFace,
+    sadFaceColor,
+    neutral,
+    neutralColor,
+    smile,
+    smileColor,
+    happy,
+    happyColor,
 };
