dfe562473352bddda7e97a32ea28f96eddbd46c6
yevGen-S <yevgeniysudakov@gmail.com>
2024-11-09 03:29:34 +0300

    feat: add sign in sign up

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,19 +1,58 @@
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 import React, { useState } from 'react';
 import { Input } from '@rneui/themed';
 import CustomButton from '../CustomButton/CustomButton';
 import { router } from 'expo-router';
+import { supabase } from '@/supabase';
+
+type authProps = {
+    email: string;
+    password: string;
+};
+
+export const signIn = async ({ email, password }: authProps) => {
+    const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+
+    if (error) {
+        Alert.alert(error.message);
+        return;
+    }
+    router.navigate('/main');
+};
+
+export const signUp = async ({ email, password }: authProps) => {
+    const {
+        data: { session },
+        error,
+    } = await supabase.auth.signUp({
+        email,
+        password,
+    });
+
+    if (error) {
+        Alert.alert(error.message);
+        return;
+    }
+    if (!session) {
+        Alert.alert('Проверьте почтовый ящик!');
+        return;
+    }
+    router.navigate('../(auth)/signIn');
+};
 
 const Form = ({ auth }: { auth: 'SignIn' | 'SignUp' }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     return (
-        <View style={{ width: '80%' }}>
+        <View style={{ width: '80%', alignItems: 'center' }}>
             <View style={[styles.verticallySpaced, styles.mt20]}>
                 <Input
                     leftIcon={{ type: 'font-awesome', name: 'envelope' }}
-                    onChangeText={(text) => setEmail(text)}
+                    onChangeText={setEmail}
                     value={email}
                     placeholder="E-mail / Логин"
                     autoCapitalize={'none'}
@@ -22,7 +61,7 @@ const Form = ({ auth }: { auth: 'SignIn' | 'SignUp' }) => {
             <View style={[styles.verticallySpaced]}>
                 <Input
                     leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                    onChangeText={(text) => setPassword(text)}
+                    onChangeText={setPassword}
                     value={password}
                     secureTextEntry={true}
                     placeholder="Пароль"
@@ -32,14 +71,18 @@ const Form = ({ auth }: { auth: 'SignIn' | 'SignUp' }) => {
             {auth === 'SignIn' && (
                 <CustomButton
                     showText="Войти"
-                    onPress={() => router.navigate('/main')}
+                    onPress={() => {
+                        signIn({ email, password });
+                    }}
                     style={{ marginBottom: 10 }}
                 />
             )}
             {auth === 'SignUp' && (
                 <CustomButton
                     showText="Создать аккаунт"
-                    onPress={() => router.navigate('/main')}
+                    onPress={() => {
+                        signUp({ email, password });
+                    }}
                     style={{ marginBottom: 10 }}
                 />
             )}
--- supabase.ts
+++ supabase.ts
@@ -1,14 +1,14 @@
 import { AppState } from 'react-native';
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const supabase = createClient(
     process.env.EXPO_PUBLIC_API_URL!,
     process.env.EXPO_PUBLIC_API_KEY!,
     {
         auth: {
-            // storage: AsyncStorage,
+            storage: AsyncStorage,
             autoRefreshToken: true,
             persistSession: true,
             detectSessionInUrl: true,
